be679faf367bc160986be784fc4fae8a4302b93d
Cecilius Sousa <ceciliusousa@gmail.com>
2024-06-24 16:32:28 -0300

    Cards de produtos completos

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,4 +1,5 @@
 import { ProductType } from '@/types/ProductsType';
+import ProductImage from './ProductImage';
 
 type ProductProps = {
   product: ProductType;
@@ -6,10 +7,20 @@ type ProductProps = {
 
 export default function Product({ product }: ProductProps) {
   return (
-    <div className='flex flex-col shadow-lg h-96 bg-slate-800 p-3 text-gray-300'>
-      <div className='relative max-h-72 flex-1 ' >IMG</div>
-      <div className='flex justify-between font-bold my-3 ' > {product.title} </div>
-      <button className='rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center' > Adicionar ao carrinho </button>
+    <div className='rounded-md   flex flex-col shadow-lg h-96 bg-slate-800 p-3 text-gray-300'>
+      <div className='relative max-h-72 flex-1 '>
+        <ProductImage product={product} fill></ProductImage>
+      </div>
+
+      <div className='flex justify-between font-bold my-3 '>
+        {' '}
+        <p className='w-40 truncate'>{product.title} </p>
+        <p className='text-md text-teal-600'>{product.price}</p>
+      </div>
+      <button className='rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center'>
+        {' '}
+        Adicionar ao carrinho{' '}
+      </button>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
     <div className='max-w-5xl mx-auto pt-8 px-8 xl:px-0'>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6 '>
         {products.map((product: ProductType) => (
-          <Product key={product.id} product={product} ></Product>
+          <Product key={product.id} product={product}></Product>
         ))}
       </div>
     </div>
